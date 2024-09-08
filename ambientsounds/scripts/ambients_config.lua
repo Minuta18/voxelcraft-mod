811bd136b2f64526b20c28d5b562dae8c9d0837c
Igor Samsonov <95094296+Minuta18@users.noreply.github.com>
2024-09-08 11:56:19 +0300

    Delete ambientsounds directory

--- ambientsounds/scripts/ambients_config.lua
+++
@@ -1,6 +0,0 @@
-return
-{
-	["beach-ambients"] = true,
-	["ambients-volume"] = 1.0,
-	["underwater-sounds-volume"] = 1.0
-}
--- ambientsounds/scripts/hud.lua
+++
@@ -1,3 +0,0 @@
-function on_hud_open(pid)
-	load_script("ambientsounds:scripts/ambients_player.lua").pid = pid
-end
